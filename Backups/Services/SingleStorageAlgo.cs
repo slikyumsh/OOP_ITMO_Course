869782a328147aeda5908da8bb20541e50ee80b0
Dmitrii <e.dim.v@icloud.com>
2021-11-02 17:20:10 +0300

    fix

--- Backups/Services/SingleStorageAlgo.cs
+++ Backups/Services/SingleStorageAlgo.cs
@@ -11,8 +11,8 @@ namespace Backups
         {
             if (list.Count == 0)
                 throw new ArgumentException("List is empty");
-            string zipFile = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent?.FullName + "/Backups/BackupWorkFiles/ZipFile";
-            RestorePoint restorePoint = new RestorePoint(Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent?.FullName + "/Backups/BackupWorkFiles");
+            string zipFile = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName + "/Backups/BackupWorkFiles/ZipFile";
+            RestorePoint restorePoint = new RestorePoint(Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName + "/Backups/BackupWorkFiles");
             using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
             {
                 foreach (var file in list)
