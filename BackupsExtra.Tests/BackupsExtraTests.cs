0d1e3a7c40f7f7151ec61d7061610a76920c49e3
Dmitrii <e.dim.v@icloud.com>
2021-12-01 18:27:17 +0300

    test: test added

--- BackupsExtra.Tests/BackupsExtraTests.cs
+++ BackupsExtra.Tests/BackupsExtraTests.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+using System.IO;
+using Backups;
+using NUnit.Framework;
+
+namespace BackupsExtra.Tests
+{
+    public class BackupsExtraTests
+    {
+        [Test]
+        public void TestDownloadAndSave()
+        {
+            Directory.CreateDirectory(
+                Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent?.FullName +
+                "/BackupsExtra/WorkFiles/");
+            FileStream fileStream1 = File.Create(
+                Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent?.FullName +
+                "/BackupsExtra/WorkFiles/1.txt");
+            fileStream1.Close();
+            Directory.CreateDirectory(
+                Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent?.FullName +
+                "/BackupsExtra/BackupWorkFiles/");
+            BackupExtraService service = new BackupExtraService();
+            Assert.AreEqual(0,service.TestNumberOfDownloadedBackupJobs());
+            service.Download();
+            int x = service.TestNumberOfDownloadedBackupJobs();
+            BackupJob job = service.CreateBackupJob(new SingleStorageAlgo(), new Repository(Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent?.FullName + "/BackupsExtra/BackupWorkFiles/"), new ConsoleLogger());
+            service.Save();
+            int y = service.TestNumberOfDownloadedBackupJobs();
+            Assert.AreEqual(1, y - x);
+            Directory.Delete(Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent?.FullName + "/BackupsExtra/WorkFiles/", true);
+            Directory.Delete(Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent?.FullName + "/BackupsExtra/BackupWorkFiles/", true);
+
+        }
+    }
+}
