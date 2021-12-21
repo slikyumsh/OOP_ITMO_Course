5425abef037146663c8a434d6fcbb3539866153c
Dmitrii <e.dim.v@icloud.com>
2021-12-22 01:50:47 +0300

    fix: private date removed from dto

--- Report.Models/EmployeeDto.cs
+++ Report.Models/EmployeeDto.cs
@@ -6,7 +6,5 @@ namespace Report.Models
     {
         public string Name { get; set; }
         public string BossName { get; set; }
-        public Guid Id { get; set; }
-        public Guid ReportId { get; set; }
     }
 }
--- Report.Models/TaskDto.cs
+++ Report.Models/TaskDto.cs
@@ -10,6 +10,5 @@ namespace Report.Models
         public DateTime CreationTime { get; set; }
         public DateTime ModificationTime { get; set; }
         public string Comment { get; set; }
-        public Guid Id { get; set; }
     }
 }
