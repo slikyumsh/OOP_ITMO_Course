01956535845e9cb0cef21c42cd8c9167b8f37357
Dmitrii <e.dim.v@icloud.com>
2021-12-21 21:59:17 +0300

    feat: DTO models added

---
+++ Report.Models/EmployeeDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Report.Models
+{
+    public class EmployeeDto
+    {
+        public string Name { get; set; }
+        public string BossName { get; set; }
+        public Guid Id { get; set; }
+        public Guid ReportId { get; set; }
+    }
+}
---
+++ Report.Models/TaskDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Report.Models
+{
+    public class TaskDto
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string Status { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime ModificationTime { get; set; }
+        public string Comment { get; set; }
+        public Guid Id { get; set; }
+    }
+}
